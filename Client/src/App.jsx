611e0fee515a55866a98a8bc63ed072e1289daed
hiarun01 <hiarun02@gmail.com>
2025-06-15 19:55:38 +0530

    init project

---
+++ Client/src/App.jsx
@@ -0,0 +1,12 @@
+import {Button} from "@/components/ui/button";
+
+const App = () => {
+  return (
+    <div className="flex min-h-svh flex-col items-center justify-center">
+      TwiFake : a fake x tweet generator
+      <Button>Click me</Button>
+    </div>
+  );
+};
+
+export default App;
